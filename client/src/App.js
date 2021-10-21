4b7fc8c6db07ea04de42f691f44d6c753e61f223
dilipksahu <sahud048@gmail.com>
2021-10-21 21:53:33 +0530

    client side react project setup

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { Container, Typography } from '@material-ui/core';
+
+function App() {
+  return (
+    <Container maxWidth="md">
+      <Typography
+        variant="h2"
+        align="center">
+        Restaurant App
+      </Typography>
+
+    </Container>
+  );
+}
+
+export default App;
